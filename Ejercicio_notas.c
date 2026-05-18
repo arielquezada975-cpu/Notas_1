fd493c6b9059903ecadcf87cbf3c04486ddcf74c
MrGato <arielquezada975@gmail.com>
2026-05-18 12:12:11 -0500

    Prototipo de la funcion promedio

---
+++ Ejercicio_notas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+float calcularPromedio(float *vector,int tamanio);
+int main()
+ {
+    float notas[5]={6,7,7.5,6.5,8};
+        int tamanio=5;
+
+    for(int i=0;i<5;i++)
+    {
+        printf("%. 2f\n", notas[i]);
+
+    }
+
+    calcularPromedio(notas,tamanio);
+
+
+}

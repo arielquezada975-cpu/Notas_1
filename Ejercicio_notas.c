ec0eed07e4b8879b3429f4561e17dec3a51fd341
MrGato <arielquezada975@gmail.com>
2026-05-18 12:21:13 -0500

    Calcular promedio suma y vector

--- Ejercicio_notas.c
+++ Ejercicio_notas.c
@@ -12,6 +12,19 @@ int main()
     }
 
     calcularPromedio(notas,tamanio);
+    printf("El promedio es: %.2f", calcularPromedio(notas,tamanio));
+     
 
+}
+float calcularPromedio(float *vector,int tamanio)
 
+{
+    float suma, promedio =0;
+
+    for(int i=0;i<tamanio;i++) 
+    {
+        suma=suma+vector[i]; //suma+=vector[i];
+    }
+    promedio=suma/tamanio;  
+    return promedio;
 }
